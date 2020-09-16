94941067df26657a4ef829e9112e628d93fc3579
Valentin Machefaux <68844465+Valentin-devweb@users.noreply.github.com>
2020-09-16 11:11:11 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// NAVIGATION
 function navBurger() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,35 +7,23 @@ function navBurger() {
       x.className = "topnav";
     }
 }
-$(function(){
-  // nombre de li pour #question
-  let length = $('#question > *').length;  
-  // Boucle pour parcourir les a des li et 
-  for (let index = 1; index < length+1; index++) {
-    // récupérer la valeur de l'attribut href
-    let target = $('#question li:nth-child('+index+') a').attr('href');    
-    // si a cliqué
-    $('#question li:nth-child('+index+') a').click(function() {
-      // appel fonction de supprétion classe backcolor
-      removeAll(length);
-      // la réponse surlignée via classe backcolor
-      $(target).addClass('backcolor');
-    });
-    // fonction qui supprime la classe backcolor si déja attribuée
-    function removeAll(l){
-      // parcourir les réponses une par une
-      for (let index = 1; index < l+1; index++) {
-        // supprimer la classe backcolor si elle a été attribuée
-        $('#reponse li:nth-child('+index+') ').removeClass('backcolor');
-      };
-    } ;
-  }; // fin du for
-});
 
-// // 
-// var question = document.getElementById('question');
-// question.addEventListener('click', function(e) {
-//   if (e.target.id=='0'){
-//     document.getElementById('premiere').setAttribute('class', 'backcolor')
-//   }
-// });
+// GO TO TOP
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
